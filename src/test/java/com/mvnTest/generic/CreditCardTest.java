99ac298d2f773b589571b0a1dc43ca9888919f0f
kailash <kailash007ranchi@gmail.com>
2020-01-01 14:05:39 +0530

    codes added for screenshot

--- src/test/java/com/mvnTest/generic/CreditCardTest.java
+++ src/test/java/com/mvnTest/generic/CreditCardTest.java
@@ -1,8 +1,16 @@
 package com.mvnTest.generic;
 
+import java.io.File;
+import java.util.Date;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.testng.annotations.Test;
+
+import genericLib.BaseClass;
 
 public class CreditCardTest {
 	
@@ -15,6 +23,28 @@ public class CreditCardTest {
 	// Open Google
 	driver.get("http://www.google.com");
 	 
+	
+//taking screenshot
+
+
+	Date date=new Date();
+	String currentDate=date.toString().replace(":", "_").replace(" ", "_");
+	
+	String failedTestName=getClass().toString();
+	System.out.println("--------failed----"+failedTestName);
+	EventFiringWebDriver eDriver=new EventFiringWebDriver(driver);
+	File srcFile=eDriver.getScreenshotAs(OutputType.FILE);
+	File destFile=new File("./screenshot/"+failedTestName+"_"+currentDate+".png");
+	try{
+		FileUtils.copyFile(srcFile, destFile);
+		System.out.println("screenhot has captured");
+	}
+	catch (Exception e) {
+		//
+		System.out.println("screenhot not captured , am in catch blocks");
+	}
+		
+	Thread.sleep(3000); 
 	// Close browser
 	driver.quit();
 	}
